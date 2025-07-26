0a11b7c04744adde13399072e27078aab7ef759c
Sunil Suryavanshi <nirajsuryavanshi03@gmail.com>
2025-07-26 23:11:22 +0530

    Add styles and structure for About, Contact, Projects, Services, and Hero sections

--- src/app/sections/hero/hero.ts
+++ src/app/sections/hero/hero.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
   templateUrl: './hero.html',
   styleUrl: './hero.css'
 })
 export class Hero {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
